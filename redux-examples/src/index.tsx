9d0655fb6901a3fd82597f80b4083cf35e4ad4a8
Ngan Tong Kim <ngan.tongkim@asnet.com.vn>
2020-06-15 09:53:33 +0700

    Example for redux devtools

--- redux-examples/src/index.tsx
+++ redux-examples/src/index.tsx
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 import App from './App';
 import './index.css';
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <React.StrictMode>
